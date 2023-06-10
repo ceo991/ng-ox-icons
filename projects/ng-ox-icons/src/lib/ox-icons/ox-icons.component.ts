bf7dfbddaad8f45606e38775405f18e2de266dc8
ceo991 <ceo991@hotmail.com>
2023-06-10 13:05:47 +0300

    ng-ox-icons initial commit

--- projects/ng-ox-icons/src/lib/ox-icons/ox-icons.component.ts
+++ projects/ng-ox-icons/src/lib/ox-icons/ox-icons.component.ts
@@ -1,22 +1,32 @@
-import { ChangeDetectionStrategy, Component, ElementRef, Inject, Input, OnInit, Optional, ViewChild } from '@angular/core';
+import {
+  AfterContentInit, AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Inject,
+  Input,
+  OnInit,
+  Optional,
+  ViewChild
+} from '@angular/core';
+// tslint:disable-next-line:import-spacing
 import { DOCUMENT }                                                                                   from '@angular/common';
 import { oxIcons, OxIcons } from 'ox-icons';
 
 import { OxIconsRegistry } from './ox-icons-registry.service';
 
 @Component({
+  // tslint:disable-next-line:component-selector
     selector: 'ox-icons',
     template: `
-      <span #text style="display: none">
-        <ng-content></ng-content>
-      </span>
+      <ng-content></ng-content>
     `,
     styles: [':host::ng-deep svg{width: 20px; height: 20px}'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OxIconsComponent implements  OnInit{
-    private svgIcon: SVGElement | undefined;
-    @ViewChild('text') icon: ElementRef | undefined ;
+export class OxIconsComponent {
+    private svgIcon: SVGElement | undefined | string = undefined;
+    // @ViewChild('text') icon: ElementRef | undefined ;
     @Input()
     set name(iconName: oxIcons) {
         if (this.svgIcon) {
@@ -34,20 +44,20 @@ export class OxIconsComponent implements  OnInit{
                 @Optional() @Inject(DOCUMENT) private document: any) {
     }
 
-    ngOnInit(): void {
-      if (!this.icon){
-        return;
-      }
-      this.name = this.icon.nativeElement.textContent.trim();
-      if (this.svgIcon) {
-        this.element.nativeElement.removeChild(this.svgIcon);
-      }
-      const svgData = this.oxIconsRegistry.getIcon(this.name);
-      if (svgData) {
-        this.svgIcon = this.svgElementFromString(svgData);
-        this.element.nativeElement.appendChild(this.svgIcon);
-      }
-    }
+    // ngAfterViewInit(): void{
+    //   if (!this.icon){
+    //     return;
+    //   }
+    //   this.name = this.icon.nativeElement.textContent.trim();
+    //   if (this.svgIcon) {
+    //     this.element.nativeElement.removeChild(this.svgIcon);
+    //   }
+    //   const svgData = this.oxIconsRegistry.getIcon(this.name);
+    //   if (svgData) {
+    //     this.svgIcon = this.svgElementFromString(svgData);
+    //     this.element.nativeElement.appendChild(this.svgIcon);
+    //   }
+    // }
 
   private svgElementFromString(svgContent: string): SVGElement {
         const div = this.document.createElement('DIV');
